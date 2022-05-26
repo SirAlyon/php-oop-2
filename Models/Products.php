4b988e3fe86557af6b9db9b3de9294a877b22314
SirAlyon <alessiosada04@gmail.com>
2022-05-26 17:08:00 +0200

    add setDiscount and getFinalPrice methods

--- Models/Products.php
+++ Models/Products.php
@@ -1,16 +1,17 @@
 <?php
 class Products {
     public $price;
+    public $final_price;
     public $quantity;
     public $title;
     public $description;
     public $category;
     public $image;
     
-
     function __construct(Int $price, Int $quantity, String $title, String $description, Array $category, String $image, Int $discount = 0)
     {
         $this->price = $price;
+        $this->final_price = $price;
         $this->quantity = $quantity;
         $this->title = $title;
         $this->description = $description;
@@ -22,6 +23,14 @@ class Products {
     {
         if ($user !== null){
             $this->discount = 20;
+            $this->getFinalPrice();
         }
+        
     }
+
+    public function getFinalPrice()
+    {
+        $this->final_price = $this->price - ($this->price *20 / 100);
+    }
+
 }
--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
-
+$active_user = null;
 include __DIR__ .'/Models/Products.php';
 include __DIR__ .'/Models/Food.php';
 include __DIR__ .'/Models/Kennel.php';
 include __DIR__ .'/Models/User.php';
 
+$products = [
+    new Food(50, 100, 'Crocchette Pesce e Patate', 'Lorem', ['dog'], 'https://picsum.photos/200/300', 14),
+    new Kennel(200, 100, 'Kennel Integrale', 'Lorem', ['dog'], 'https://picsum.photos/200/300', 'Kennel', 'XL', 10),
+];
 
 
 
-$crocchette_1 = new Food(50, 100, 'Crocchette Pesce e Patate', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, maxime ducimus! Neque temporibus iure dignissimos, aliquam fugit harum animi aperiam?', ['dog'], 'https://picsum.photos/200/300', 14);
-$kennel_1 = new Kennel(200, 100, 'Kennel Integrale', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, maxime ducimus! Neque temporibus iure dignissimos, aliquam fugit harum animi aperiam?', ['dog'], 'https://picsum.photos/200/300', 'Kennel', 'XL', 10);
-$active_user = new User('SirAlyon', 'Alessio', 'Sada', 'alessiosada04@gmai.com', '234242341', 'Viale Virgilio', '1234 1234 1243 1234/12/24/131', false);
-var_dump($crocchette_1, $kennel_1, $user_1);
 
-$crocchette_1->setDiscount($active_user);
-var_dump($crocchette_1)
+//$active_user = new User('SirAlyon', 'Alessio', 'Sada', 'alessiosada04@gmai.com', '234242341', 'Viale Virgilio', '1234 1234 1243 1234/12/24/131', false);
+
+
+foreach($products as $product){
+    $product->setDiscount($active_user);
+};
+
+var_dump($products);
+
+
+
 ?>
